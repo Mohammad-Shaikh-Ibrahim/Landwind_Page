62258b1c27c33c03e8631b1e8a6f1e370641b173
Mohammad-Shaikh-Ibrahim <mohammadshaikhibrahim2002@gmail.com>
2024-12-30 23:21:24 +0200

    Colors and Fonts is Done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
+  
   theme: {
-    extend: {},
+    extend: {
+      color:{
+        'primary': '#7E3AF2',
+        'secondaryText': '#6B7280',
+        'mainText': '#111928'
+        },
+      fontFamily: {
+        Inter:[Inter, sans-serif]
+      },
+    },
   },
   plugins: [],
 }
